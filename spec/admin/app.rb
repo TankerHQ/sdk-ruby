b75eecfb06be431895e9b50d8b4ac76271343093
Quentin Vernot <quentin.vernot@doctolib.com>
2022-06-02 17:09:30 +0200

    refactor(tests): use an api token instead of the app's auth_token for verification

--- spec/admin/app.rb
+++ spec/admin/app.rb
@@ -4,21 +4,20 @@ module Tanker
   class Admin
     # Information from the Admin SDK concerning a Tanker application
     class App
-      attr_reader :admin, :id, :auth_token, :secret
+      attr_reader :admin, :id, :secret
 
-      def initialize(admin:, id:, auth_token:, secret:)
+      def initialize(admin:, id:, secret:)
         @admin = admin
         @id = id
-        @auth_token = auth_token
         @secret = secret
       end
 
       def get_email_verification_code(email)
-        @admin.get_email_verification_code(@id, @auth_token, email)
+        @admin.get_email_verification_code(@id, email)
       end
 
       def get_sms_verification_code(phone_number)
-        @admin.get_sms_verification_code(@id, @auth_token, phone_number)
+        @admin.get_sms_verification_code(@id, phone_number)
       end
     end
   end
