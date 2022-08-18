5d6792ab79b46aa4186bf90d76a8502d44c05409
Timothée Isnard <timothee.isnard@tanker.io>
2022-08-18 15:49:24 +0200

    feat: Remove reset-branch machinery (upstream sets the right branch)

--- run-ci.py
+++ run-ci.py
@@ -111,9 +111,6 @@ def main() -> None:
         dest="update",
     )
 
-    reset_branch_parser = subparsers.add_parser("reset-branch")
-    reset_branch_parser.add_argument("branch", nargs="?")
-
     download_artifacts_parser = subparsers.add_parser("download-artifacts")
     download_artifacts_parser.add_argument("--project-id", required=True)
     download_artifacts_parser.add_argument("--pipeline-id", required=True)
@@ -158,12 +155,6 @@ def main() -> None:
             deploy(args.version)
     elif command == "lint":
         lint()
-    elif command == "reset-branch":
-        fallback = os.environ["CI_COMMIT_REF_NAME"]
-        ref = tankerci.git.find_ref(
-            Path.cwd(), [f"origin/{args.branch}", f"origin/{fallback}"]
-        )
-        tankerci.git.reset(Path.cwd(), ref, clean=False)
     elif command == "download-artifacts":
         tankerci.gitlab.download_artifacts(
             project_id=args.project_id,
