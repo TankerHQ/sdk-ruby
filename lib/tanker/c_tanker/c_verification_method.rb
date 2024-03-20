50803f79855db33aa543d186c6f6b8f7349d3546
julien mounier <julien.mounier@doctolib.com>
2024-03-20 14:57:28 +0100

    feat(oidc): add OIDCAutorizationCodeVerification

--- lib/tanker/c_tanker/c_verification_method.rb
+++ lib/tanker/c_tanker/c_verification_method.rb
@@ -21,6 +21,7 @@ module Tanker
       TYPE_PREVERIFIED_PHONE_NUMBER = 7
       TYPE_E2E_PASSPHRASE = 8
       # TYPE_PREVERIFIED_OIDC = 9 # Preverified OIDC is not exposed as a VerificationMethod
+      # TYPE_OIDC_AUTHORIZATION_CODE, = 10 # OIDC authorization code is not exposed as a VerificationMethod
 
       def to_verification_method # rubocop:disable Metrics/CyclomaticComplexity Not relevant for a case/when
         case self[:type]
