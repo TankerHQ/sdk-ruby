f1febfaed95b68ea627cb59edbe9bb307c293dd8
Timothée Isnard <timothee.isnard@tanker.io>
2024-08-13 12:41:03 +0200

    WIP: Add debug logs around fork hook

--- lib/tanker/c_tanker/fork_hook.rb
+++ lib/tanker/c_tanker/fork_hook.rb
@@ -5,12 +5,14 @@ require 'tanker/core/http'
 module Tanker
   module ForkHook
     def _fork(*args)
+      puts "### TANKER-CORE BEFORE FORK HOOK"
       CTanker.tanker_before_fork
       Http::ThreadPool.before_fork
 
       res = super
 
       CTanker.tanker_after_fork
+      puts "### TANKER-CORE AFTER FORK HOOK"
       res
     end
     prepend_features(Process.singleton_class)
